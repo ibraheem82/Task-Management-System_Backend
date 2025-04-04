3cfecca3110c32de906a8de31e8c10ff415558eb
ibraheemomikunle82 <ibraheemomikunle82@gmail.com>
2025-04-04 09:28:41 +0000

    ``` feat: Configure Express middleware and API routes
    This commit configures the Express application with necessary middleware and sets up API routes.
    
    - Adds `express.json()` middleware to parse incoming JSON requests.
    - Adds `express.urlencoded({ extended: true })` middleware to parse URL-encoded requests with extended syntax.
    - Mounts the `UsersRouter` at the '/api/users/' endpoint.
    - Mounts the `TasksRouter` at the '/api/tasks' endpoint.
    ```

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import dbConnect from "../config/dbConnect.js";
 import { globalErrHandler, notFound } from '../middlewares/globalErrHandler.js';
 import UsersRouter from "../routes/user.routes.js";
+import TasksRouter from "../routes/task.routes.js";
 
 // * Database Connection.
 dbConnect();
@@ -14,7 +15,11 @@ dbConnect();
 const app = express();
 // parse incoming datas, meaning the datas coming in the {req}, will be converted as json.
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use('/api/users/', UsersRouter);
+app.use('/api/tasks', TasksRouter);
 
 
 
