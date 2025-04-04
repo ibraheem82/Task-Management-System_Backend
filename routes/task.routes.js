1e92db992387d64c6c2215941444f791d665123b
ibraheemomikunle82 <ibraheemomikunle82@gmail.com>
2025-04-04 09:27:10 +0000

    added middeware to make sure is is only the authenticated admin that can access the admin route...

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -5,15 +5,15 @@ import tasksController from '../controllers/task.controller.js';
 import taskValidators from '../validators/task.js';
 import isUserLoggedIn from '../middlewares/isUserLoggedIn.js';
 import upload from '../config/multerConfig.js';
-
+import isUser from '../middlewares/isUser.js';
 
 const TasksRouter = express.Router();
 
 
-TasksRouter.post('', isUserLoggedIn, upload, taskValidators.createTaskValidator, validate, tasksController.createTask);
-TasksRouter.put('/:taskId', isUserLoggedIn, tasksController.updateTask);
-TasksRouter.delete("/:taskId", isUserLoggedIn, tasksController.deleteTask);
-TasksRouter.patch("/:taskId/status", isUserLoggedIn, tasksController.updateTaskStatus);
+TasksRouter.post('', isUserLoggedIn, isUser,upload, taskValidators.createTaskValidator, validate, tasksController.createTask);
+TasksRouter.put('/:taskId', isUserLoggedIn, isUser, tasksController.updateTask);
+TasksRouter.delete("/:taskId", isUserLoggedIn, isUser, tasksController.deleteTask);
+TasksRouter.patch("/:taskId/status", isUserLoggedIn, isUser, tasksController.updateTaskStatus);
 
 
 export default TasksRouter;
