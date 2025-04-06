f23f6be455246c294c786155e9b2360242308117
ibraheemomikunle82 <ibraheemomikunle82@gmail.com>
2025-04-06 16:25:27 +0000

    assign user task route done...

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -6,6 +6,7 @@ import taskValidators from '../validators/task.js';
 import isUserLoggedIn from '../middlewares/isUserLoggedIn.js';
 import upload from '../config/multerConfig.js';
 import isUser from '../middlewares/isUser.js';
+import isLoggedIn from '../middlewares/isLoggedIn.js';
 
 const TasksRouter = express.Router();
 
@@ -14,6 +15,7 @@ TasksRouter.post('', isUserLoggedIn, isUser,upload, taskValidators.createTaskVal
 TasksRouter.put('/:taskId', isUserLoggedIn, isUser, tasksController.updateTask);
 TasksRouter.delete("/:taskId", isUserLoggedIn, isUser, tasksController.deleteTask);
 TasksRouter.patch("/:taskId/status", isUserLoggedIn, isUser, tasksController.updateTaskStatus);
+TasksRouter.put("/:taskId/assign/:userId", isLoggedIn, tasksController.assignTask);
 
 
 export default TasksRouter;
