c3e9dbefdaabcffb6f08569894a56d9c2c20dca5
ibraheemomikunle82 <ibraheemomikunle82@gmail.com>
2025-04-06 17:09:47 +0000

    ``` feat: Configure Cloudinary for image/file uploads
    This commit sets up the Cloudinary configuration using the provided cloud name, API key, and API secret. This configuration will be used for uploading and managing images and other files in the application.
    ```

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,10 @@
+import cloudinary from 'cloudinary';
+
+// Set up Cloudinary configuration
+cloudinary.config({
+  cloud_name: "doejgwpnr",
+  api_key:  "153669577219317",
+  api_secret: "qsUs4Zlf7Au1hGCBcTmUuO42gYw"
+});
+
+export default cloudinary;
